f470b4994bb6d454402b9d71b774655b8c563e3c
Adam Birse <adambirse@gmail.com>
2018-03-03 16:05:42 +0000

    Another service added.

---
+++ dependency-one/src/test/java/com/worth/warp/LongRunningTests.java
@@ -0,0 +1,17 @@
+package com.worth.warp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class LongRunningTests {
+
+    @Test
+    public void testSomethingUseful() throws InterruptedException {
+       Thread.sleep(5000);
+    }
+
+}
---
+++ simple-service-no-dependencies/src/main/java/com/worth/warp/SimpleController.java
@@ -0,0 +1,19 @@
+package com.worth.warp;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("/simple")
+public class SimpleController {
+
+    private static final Log LOG = LogFactory.getLog(SimpleController.class);
+
+
+    @GetMapping
+    public String hello() {
+        LOG.info("Saying hello with no dependencies");
+        return "hello with no dependencies";
+    }
+}
